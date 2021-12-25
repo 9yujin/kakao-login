143e025ea00ce0f5f1233932836741fd49d13111
한규진 <david0218@naver.com>
2021-12-25 11:28:09 +0900

    배포

--- src/context/index.js
+++ src/context/index.js
@@ -2,7 +2,7 @@ import React, { createContext, useState } from "react";
 
 const userContext = createContext({
     kakao: { name: "", email: "", id: "", image: "" },
-    user: { name: "", email: "" },
+    user: { name: "", email: "", wow: "", image: "", phone: "" },
     setKakao: () => {},
     setUser: () => {},
 });
